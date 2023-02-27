9bb6461cbc20260d8c3c5ccf80b245fc01b9154e
Clay Ashby <clay.ashby@gmail.com>
2023-02-27 13:01:46 -0500

    Added <ul> list for Topics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,13 +25,53 @@ const Home: NextPage = () => {
 export default Home;
 
 const Content: React.FC = () => {
-  const { data: sessionData } = useSession();
+  const { data: sessionData } = useSession(); // is this used?
 
   const { data: topics, refetch: refectTopics } = api.topic.getAll.useQuery(
+    // is this used?
     undefined, // no input
     {
-      enabled: sessionData?.user !== undefined,
+      enabled: sessionData?.user !== undefined, // is this used?
     }
   );
-  return <div>{JSON.stringify(topics)}</div>;
+
+  const createTopic = api.topic.create.useMutation({});
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider"></div>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  );
 };
+
+// <div>{JSON.stringify(topics)}</div>;    // is this used?
