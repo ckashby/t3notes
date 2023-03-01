527c6e7d7dc3b34381cac483ac8050c9831bf444
Clay Ashby <clay.ashby@gmail.com>
2023-03-01 12:45:12 -0500

    Create NoteEditor comp

---
+++ src/components/NoteEditor.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+
+export const NoteEditor = ({
+  onSave,
+}: {
+  onSave: (note: { title: string; content: string }) => void;
+}) => {
+  const [code, setCode] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+
+  return (
+    <div className="border-grey-200 card mt-5 border bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">
+          <input
+            type="text"
+            placeholder="Note title"
+            className="input-primary input input-lg w-full font-bold"
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}
+          />
+        </h2>
+      </div>
+    </div>
+  );
+};
